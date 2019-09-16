9456f0eea0c132190d9f8f58939d98f24e6f1e8f
Kaur Kaelep <kaur.kaelep@khk.ee>
2019-09-16 14:42:52 +0300

    twigi intregeerimine

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -5,15 +5,13 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
-class IndexController
+class IndexController extends AbstractController
 {
      /**
      * @Route("/")
      */
     public function index()
     {
-        return new Response(
-            '<html><body><h1>Booking App index page</h1></body></html>'
-        );
+        return $this->render('index.html.twig');
     }
 }
