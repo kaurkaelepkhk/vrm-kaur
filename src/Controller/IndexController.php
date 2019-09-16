5792f39069059b3ae889ec04e46b093ea41b633e
Kaur Kaelep <kaur.kaelep@khk.ee>
2019-09-16 15:53:03 +0300

    add item to database

--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -1,9 +1,13 @@
 <?php
 // src/Controller/IndexController.php
 namespace App\Controller;
+
+use App\Entity\Bookings;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Doctrine\DBAL\Driver\Connection;
+use Doctrine\ORM\EntityManagerInterface;
 
 class IndexController extends AbstractController
 {
@@ -13,5 +17,28 @@ class IndexController extends AbstractController
     public function index()
     {
         return $this->render('index.html.twig');
+    }
+     /**
+     * @Route("/bookings/create")
+     */
+    public function create(EntityManagerInterface $em)
+    {
+        $booking = new Bookings();
+        $booking->setFirstName('Kadi');
+        $booking->setLastName('Tamm');
+        $booking->setPhone('55555555');
+        $booking->setEmail('kadi.tamm@test.ee');
+        $booking->setBirthdate(new \DateTime('1995-09-01'));
+        $booking->setStartDate(new \DateTime('2019-09-16'));
+        $booking->setEndDate(new \DateTime('2019-09-18'));
+        $booking->setArrivalTime(new \DateTime('14:00'));
+        $booking->setNumberOfPeople(2);
+        $booking->setAdditionaInformation('mingi oluline info');
+        $booking->setPayingMethod('cash');
+        $em->persist($booking);
+        $em->flush();
+        return new Response(
+            '<html><body><h1>'.$booking->getFirstName().' '.$booking->getLastName().', booking data is saved.</h1></body></html>'
+        );
     }
 }
